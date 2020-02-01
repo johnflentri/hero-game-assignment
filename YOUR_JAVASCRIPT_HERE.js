70b63a55a894c044fe8d4d4b1398975fe73543e3
johnflentri <jpflentri@gmail.com>
2020-02-01 19:56:51 +0100

    All functions are working effectively. I had to comment out the removal of the dagger image as it was interfering with run test button

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,5 +1,7 @@
 const hero = { name: 'Shrek', heroic: true, inventory: [], health: 10, weapon: { type: 'club', damage: 2 } };
-const villain = { name: 'Lord Farquaad', heroic: false, inventory: [], health: 2, weapon: { type: 'fist', damage: 1 } };
+const villain1 = { name: 'Prince Charming', heroic: false, inventory: [], health: 2, weapon: { type: 'Sword', damage: 5 } };
+const villain2 = { name: 'Lord Farquaad', heroic: false, inventory: [], health: 2, weapon: { type: 'fist', damage: 5 } };
+
 
 
 function rest(person) {
@@ -13,9 +15,12 @@ function rest(person) {
 }
 
 const weapon = { type: 'dagger', damage: 2 }
+const weapon2 = { type: 'club', damage: 2 }
 
 function pickUpItem(person, weapon) {
   person.inventory.push(weapon)
+  // const removeDagger = document.getElementById("dagger")
+  // removeDagger.parentNode.removeChild(removeDagger)
   displayStats()
   return person
 }
@@ -44,24 +49,49 @@ function changeName() {
 function displayStats() {
   const dataSection = document.getElementById("heroData")
   const statsPara = document.getElementById('statsPara')
-  statsPara.innerHTML = (`Your hero is called ${hero.name}. He/She has ${hero.health} health and has a ${hero.weapon.type} that deals ${hero.weapon.damage} damage.`)
+  statsPara.innerHTML = (`Your hero is called ${hero.name}. He has ${hero.health} health is currently equipped with a ${hero.weapon.type} that deals ${hero.weapon.damage} damage.`)
   dataSection.appendChild(statsPara)
 }
 
-// const randomNumber = Math.floor(Math.random() * 2) + 1
-
-function fightEnemy() {
-  villain.health -= hero.weapon.damage
-  hero.health -= villain.weapon.damage
-  removeElement()
-  displayStats()
+function fightEnemy(enemy) {
+  if (hero.health <= enemy.weapon.damage) {
+    alert(`${hero.name} is too weak to fight this enemy, perhaps we should rest.`)
+  } else {
+    enemy.health -= hero.weapon.damage
+    hero.health -= enemy.weapon.damage
+    displayStats()
+    if (enemy === villain1 && hero.weapon !== weapon) {
+      alert(`Ouch! ${enemy.name} has inflicted ${enemy.weapon.damage} points of damage to ${hero.name}. Fortunately we have defeated him!`)
+      removeElement1()
+    } else if (enemy === villain1 && hero.weapon === weapon) {
+      return alert(`Ouch! ${enemy.name} has inflicted ${enemy.weapon.damage} points of damage to ${hero.name}. Unfortunately our ${weapon.type} was no match for his sword.`)
+    } else if (enemy === villain2 && hero.weapon !== weapon) {
+      alert(`Oh no! ${enemy.name} was able to duck under the swing of our club and kicked ${hero.name} in the shin, dealing ${enemy.weapon.damage} damage.`)
+    } else if (enemy === villain2 && hero.weapon === weapon) {
+      alert(`Hooray! ${hero.name} managed to defeat ${enemy.name} by stabbing him right in the eye. ${hero.name} will live happily ever after. Congratulations!`)
+      removeElement2()
+    }
+  }
 }
 
-function removeElement() {
-  const enemyFarquaad = document.getElementById("enemyFarquaad")
-  enemyFarquaad.parentNode.removeChild(enemyFarquaad)
+function removeElement1() {
+  const enemyToBeRemoved = document.getElementById('villain1')
+  enemyToBeRemoved.parentNode.removeChild(enemyToBeRemoved)
+}
+
+function removeElement2() {
+  const enemyToBeRemoved = document.getElementById('villain2')
+  const enemyTerritory = document.getElementById("enemyTerritory")
+  const img = document.createElement('img')
+  img.src = "images/shrekHappy.png"
+  img.className = "img"
+  enemyToBeRemoved.parentNode.removeChild(enemyToBeRemoved)
+  enemyTerritory.appendChild(img)
+  document.body.appendChild(enemyTerritory)
+}
+
+function howToWin() {
+  document.getElementById("howToWin").innerHTML = `Defeat Prince Charming first with your club, then equip the dagger to finish off Farquaad!`
 }
 
 displayStats();
-
-
