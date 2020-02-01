7fa59b3215fd8abb76ff6d03b36e8c8aa1169ec8
johnflentri <jpflentri@gmail.com>
2020-02-01 10:36:24 +0100

    Some logic and images added

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,19 @@
 const hero = { name: 'Shrek', heroic: true, inventory: [], health: 10, weapon: { type: 'Club', damage: 2 } };
+
+function rest(person) {
+  if (person.health === 10) {
+    return alert('Health already at maximum!')
+  } else {
+    person.health = 10
+    return person
+  }
+}
+
+const weapon = { type: 'dagger', damage: 2 }
+
+function pickUpItem(person, weapon) {
+  person.inventory.push(weapon)
+  return person
+}
+
+function equipWeapon() { }
